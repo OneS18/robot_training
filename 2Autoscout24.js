e0b24ba3043d1f3f132a0f75e6bb49ed80d156a5
Sergiu <70413720+OneS18@users.noreply.github.com>
2021-11-01 13:29:38 -0700

    Update 2Autoscout24.js

--- 2Autoscout24.js
+++ 2Autoscout24.js
@@ -1,43 +1,39 @@
-const cheerio = require('cheerio');
-const { toArray } = require('cheerio/lib/api/traversing');
-const Nightmare = require('nightmare')
-const { click, wait } = require('nightmare/lib/actions')
-const nightmare = Nightmare({ show: true,width: 1200, height: 900})
+const cheerio = require("cheerio");
+const { toArray } = require("cheerio/lib/api/traversing");
+const Nightmare = require("nightmare");
+const { click, wait } = require("nightmare/lib/actions");
+const nightmare = Nightmare({ show: true, width: 1200, height: 1200 });
 
 async function firstCar() {
-    await nightmare
-    .goto('https://www.autoscout24.ro')
-    .click('.search-mask-refine-link')
-    .wait(3000)
-    .click('.sc-grid-col-6')
-    .wait(3000)
-    .click('#country-input-suggestion-2')
+  await nightmare
+    .goto("https://www.autoscout24.ro")
+    .click(".search-mask-refine-link")
     .wait(1500)
-    .click(".css-1xfaiyg")
-    .wait(3000)
+    .click("button.css-1xfaiyg")
+    .wait(2000);
 
+  const body = await nightmare.evaluate(() => document.body.innerHTML);
+  const $ = cheerio.load(body);
 
-
-    const body = await nightmare. evaluate(() => document.body.innerHTML)
-    const $ = cheerio.load(body)
-
-    const car = $('.css-qdkdg').map((index, elem) => {
-      const title = $(elem).find('.css-4u347z').text()
-      const mileage = $(elem).find('.css-147zjea').text()
-      const engineSize = $(elem).find('.css-h81uhl').text()
-      const price = $(elem).find('.css-113e8xo').text()
-      const year = $(elem).find('.css-147zjea').text()
+  const car = $(".css-qdkdg")
+    .map((index, elem) => {
+      const model = $(elem).find(".css-4u347z").text();
+      const mileage = $(elem).find(".css-147zjea").text();
+      const engineSize = $(elem).find(".css-h81uhl").text().trim();
+      const price = $(elem).find(".css-113e8xo").text();
+      const year = $(elem).find(".css-147zjea").text();
 
       return {
-          title,
-          mileage,
-          engineSize,
-          price,
-          year  
-      }
-    }).toArray()
+        model,
+        mileage,
+        engineSize,
+        price,
+        year,
+      };
+    })
+    .toArray();
 
-      console.log(car)
+  console.log(car);
 }
 
-firstCar()
+firstCar();
