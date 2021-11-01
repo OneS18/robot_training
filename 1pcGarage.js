90b1a59c7e05ace6fed1545c30b7d5c61c9760cb
Sergiu <70413720+OneS18@users.noreply.github.com>
2021-11-01 15:53:03 +0200

    Update 1pcGarage.js

--- 1pcGarage.js
+++ 1pcGarage.js
@@ -1,21 +1,34 @@
-const Nightmare = require('nightmare')
+const cheerio = require('cheerio');
+const Nightmare = require('nightmare');
+const { click, evaluate } = require('nightmare/lib/actions');
+const { type } = require('os');
 const nightmare = Nightmare({ show: true,width: 1200, height: 900})
 
-nightmare
-  .goto(' https://pcgarage.ro/')
-  .type('#searchac', 'gaming mouse')
-  .click('#sf2')
-  .wait(20000)
-  .evaluate(() => {
-       let allInfo = []
-       const data = document.querySelectorAll("#wrapper_listing_products > div:nth-child(1) > div > div.product_box > div.product_box_middle > div.product_box_name > h2 > a").text;
-       allInfo = data
-       return allInfo
-    })
-  .end()
-  .then((title) => {
-        console.log(title)
-  })
-  .catch(error => {
-    console.error('Search failed:', error)
-  })
+ async function navigateToHomePage() {
+   await nightmare
+    .goto('https://pcgarage.ro/')
+    .type('#searchac', 'gaming mouse')
+    .click('#sf2')
+    .wait(20000);
+
+    const body = await nightmare. evaluate(() => document.body.innerHTML)
+    const $ = cheerio.load(body)
+    
+    const mouses = $('.product_box').map((index, elem) => {
+        const title = $(elem).find('.my-0').text();
+        const link = $(elem).find(".my-0 a").attr('href')
+        const price = $(elem).find(".price").text();
+        const sells = $(elem).find('.prefs_sort').text().trim()
+
+        return {
+            title,
+            link,
+            price,
+            sells
+        }
+    }).toArray()
+
+    console.log(mouses)
+}
+
+navigateToHomePage()
