ed3903af7fa85b7e9fba5c50318f073eef48bff7
Sergiu <70413720+OneS18@users.noreply.github.com>
2021-11-01 13:25:40 -0700

    Update 1pcGarage.js

--- 1pcGarage.js
+++ 1pcGarage.js
@@ -1,34 +1,40 @@
-const cheerio = require('cheerio');
-const Nightmare = require('nightmare');
-const { click, evaluate } = require('nightmare/lib/actions');
-const { type } = require('os');
-const nightmare = Nightmare({ show: true,width: 1200, height: 900})
+const cheerio = require("cheerio");
+const { toArray } = require("cheerio/lib/api/traversing");
+const Nightmare = require("nightmare");
+const { click, wait } = require("nightmare/lib/actions");
+const nightmare = Nightmare({ show: true, width: 1200, height: 1200 });
 
- async function navigateToHomePage() {
-   await nightmare
-    .goto('https://pcgarage.ro/')
-    .type('#searchac', 'gaming mouse')
-    .click('#sf2')
-    .wait(20000);
+async function firstCar() {
+  await nightmare
+    .goto("https://www.autoscout24.ro")
+    .click(".search-mask-refine-link")
+    .wait(1500)
+    .click("button.css-1xfaiyg")
+    .wait(2000);
 
-    const body = await nightmare. evaluate(() => document.body.innerHTML)
-    const $ = cheerio.load(body)
-    
-    const mouses = $('.product_box').map((index, elem) => {
-        const title = $(elem).find('.my-0').text();
-        const link = $(elem).find(".my-0 a").attr('href')
-        const price = $(elem).find(".price").text();
-        const sells = $(elem).find('.prefs_sort').text().trim()
+  const body = await nightmare.evaluate(() => document.body.innerHTML);
+  const $ = cheerio.load(body);
 
-        return {
-            title,
-            link,
-            price,
-            sells
-        }
-    }).toArray()
+  const car = $(".css-qdkdg")
+    .map((index, elem) => {
+      const model = $(elem).find(".css-4u347z").text();
+      const mileage = $(elem).find(".css-147zjea").text();
+      const engineSize = $(elem).find(".css-h81uhl").text().trim();
+      const price = $(elem).find(".css-113e8xo").text();
+      const year = $(elem).find(".css-147zjea").text();
 
-    console.log(mouses)
+      return {
+        model,
+        mileage,
+        engineSize,
+        price,
+        year,
+      };
+    })
+    .toArray();
+
+  console.log(car);
 }
 
-navigateToHomePage()
+firstCar();
+
