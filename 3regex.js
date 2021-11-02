4caa24db85a64f86c1aa55a86b299110c2f03c7c
Sergiu <70413720+OneS18@users.noreply.github.com>
2021-11-02 16:27:30 +0200

    Add files via upload

---
+++ 3regex.js
@@ -0,0 +1,25 @@
+//1
+/(Bills)/
+
+//2
+/[A-Z]\d{9,}/
+
+//3
+/\d\S+/
+
+//4
+/[$ | -]\d\S+/
+
+//5
+const amount = '€145.10', 
+const amount1 ='89.00€'
+
+
+console.log(amount.replace("€", ' '))
+
+
+
+
+
+
+
